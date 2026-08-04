fc631542e2d533a0345cf63ec6ee3c1b998ba043
Yongjhih Chen <yongjhih@gmail.com>
2026-08-05 01:17:18 +0800

    Enable Compose Preview tooling

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,6 +56,7 @@ dependencies {
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
     implementation("androidx.compose.material:material-icons-extended")
+    debugImplementation(libs.androidx.compose.ui.tooling)
 
     testImplementation(libs.kotlin.test)
     testImplementation(libs.junit)
