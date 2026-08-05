4b2d010115411b99d97ef081cfe9ae271aef5505
Yongjhih Chen <yongjhih@gmail.com>
2026-08-05 12:00:41 +0800

    test: Fix assertion expectations in CjkTransliteratorTest for space-separated pinyin transliteration

--- core/util/src/test/java/com/github/yongjhih/appdialer/util/CjkTransliteratorTest.kt
+++ core/util/src/test/java/com/github/yongjhih/appdialer/util/CjkTransliteratorTest.kt
@@ -11,22 +11,22 @@ class CjkTransliteratorTest {
         assertEquals("地圖", transliterator.toLatin("地圖"))
         assertEquals("カメラ", transliterator.toLatin("カメラ"))
 
-        // Default parameter injection in T9Utils
-        assertEquals("3488", "地圖".toCjkT9Full())
+        // Default parameter injection in T9Utils returns empty string for non-Latin CJK without transliterator
+        assertEquals("", "地圖".toCjkT9Full())
     }
 
     @Test
     fun testCustomCjkTransliteratorParameterInjection() {
         val customTransliterator = CjkTransliterator { text ->
             when (text) {
-                "地圖" -> "ditu"
-                "カメラ" -> "kamera"
+                "地圖" -> "di tu"
+                "カメラ" -> "ka me ra"
                 else -> text
             }
         }
 
-        assertEquals("ditu", customTransliterator.toLatin("地圖"))
-        assertEquals("kamera", customTransliterator.toLatin("カメラ"))
+        assertEquals("di tu", customTransliterator.toLatin("地圖"))
+        assertEquals("ka me ra", customTransliterator.toLatin("カメラ"))
 
         // Parameter injection into T9Utils extension functions
         assertEquals("3488", "地圖".toCjkT9Full(customTransliterator))
